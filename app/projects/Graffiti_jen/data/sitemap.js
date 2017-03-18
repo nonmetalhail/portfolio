06e88ed8d313328821b4935728d7caa79b01e660
Elliot Nahman <elliot@ischool.berkeley.edu>
2017-03-18 11:07:10 -0700

    reset

--- app/projects/Graffiti_jen/data/sitemap.js
+++
@@ -1,8 +0,0 @@
-﻿var sitemap = 
-
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,[_(c,d,e,f,g,h),_(c,i,e,f,g,j,k,[_(c,l,e,f,g,m),_(c,n,e,f,g,o),_(c,p,e,f,g,q),_(c,r,e,f,g,s),_(c,t,e,f,g,u),_(c,v,e,f,g,w),_(c,x,e,f,g,y),_(c,z,e,f,g,A),_(c,B,e,f,g,C),_(c,D,e,f,g,E),_(c,F,e,f,g,G),_(c,H,e,f,g,I)]),_(c,J,e,f,g,K,k,[_(c,L,e,f,g,M),_(c,N,e,f,g,O),_(c,P,e,f,g,Q),_(c,R,e,f,g,S),_(c,T,e,f,g,U)])]);}; 
-var b="rootNodes",c="pageName",d="loading",e="type",f="Wireframe",g="url",h="loading.html",i="graffiti",j="graffiti.html",k="children",l="sign-up",m="sign_up.html",n="viewer",o="viewer.html",p="viewer_deets",q="viewer_deets.html",r="viewer_deets_signedin",s="viewer_deets_signedin.html",t="viewer_signedin",u="viewer_signedin.html",v="artists",w="artists.html",x="user_profile",y="user_profile.html",z="home_new",A="home_new.html",B="home_new_signedin",C="home_new_signedin.html",D="map",E="map.html",F="map_signedin",G="map_signedin.html",H="home_signedin",I="home_signedin.html",J="Create",K="Create.html",L="created",M="created.html",N="Description",O="Description.html",P="color",Q="color.html",R="Brushes",S="Brushes.html",T="saved",U="saved.html";
-return _creator();
-})();
